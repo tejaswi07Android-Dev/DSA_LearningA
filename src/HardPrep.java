690690feef8ff4b3e16517478047ffa1a689431a
Tejaswi Srivastava <tejaswi.srivastava@sysquare.com>
2026-07-27 18:18:53 +0530

    office learning

--- src/HardPrep.java
+++ src/HardPrep.java
@@ -14,8 +14,34 @@ public class HardPrep {
 
 //        System.out.println(longestSubArraySum(new int[]{1,-1,0}, 0));
 
-        System.out.println(Arrays.toString(productExceptOwn(new int[]{1, 2, 3, 4})));
-        System.out.println(isValidParanthese("{}()]"));
+//        System.out.println(Arrays.toString(productExceptOwn(new int[]{1, 2, 3, 4})));
+//        System.out.println(isValidParanthese("{}()]"));
+
+        ArrayList<ArrayList<Integer>> intervals = new ArrayList<>();
+
+        intervals.add(new ArrayList<>(Arrays.asList(1, 3)));
+        intervals.add(new ArrayList<>(Arrays.asList(2, 6)));
+        intervals.add(new ArrayList<>(Arrays.asList(8, 10)));
+        intervals.add(new ArrayList<>(Arrays.asList(15, 18)));
+
+//        System.out.println(mergeList(intervals));
+
+//        System.out.println(maximumSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+
+        System.out.println(maxProfit(new int[]{7,6,5,4,2,1,7}));
+
+        int[] A = new int[]{1,1,2,3,4,4};
+        removeDuplicates(A);
+        System.out.println(Arrays.toString(A));
+//        System.out.println(Arrays.toString(moveZeroes(new int[]{5, 2, 0, 1, 0, 0, 7, 3})));
+
+//        System.out.println(uniqueElements(new int[]{1,1,2,3,5,4,2}));
+
+//        System.out.println(majority(new int[]{1,2,3,5,4,2,1,2,2,2,2,2,3}));
+
+//        System.out.println(Arrays.toString(prodExceptOwn(new int[]{1, 2, 3, 4})));
+
+        System.out.println(longestSubString("abcjackloj"));
 
     }
 
@@ -193,4 +219,198 @@ public class HardPrep {
 
     }
 
+
+    public static ArrayList<ArrayList<Integer>> mergeList(ArrayList<ArrayList<Integer>> A){
+        int a1 = A.get(0).get(0);
+        int b1 = A.get(0).get(1);
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        for(int i = 1; i < A.size(); i++){
+            int a2 = A.get(i).get(0);
+            int b2 = A.get(i).get(1);
+
+            if(b1 >= a2){
+                b1 = Math.max(b1, b2);
+            }else{
+                ArrayList<Integer> temp = new ArrayList<>();
+                temp.add(a1);
+                temp.add(b1);
+                ans.add(temp);
+
+                a1 = a2;
+                b1 = b2;
+            }
+
+
+        }
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(a1);
+        temp.add(b1);
+        ans.add(temp);
+
+        return ans;
+
+    }
+
+
+    public static int maximumSubArray(int[] A){
+        int ans = A[0];
+        int maxAns = A[0];
+
+        for(int i = 1; i < A.length; i++){
+            ans += A[i];
+            maxAns = Math.max(ans, maxAns);
+            if(ans < 0){
+                ans = 0;
+            }
+
+        }
+
+        return maxAns;
+    }
+
+
+
+
+    // ------------------------------------------------------------------------------------------------
+
+
+    public static int maxProfit(int[] A){
+        int n = A.length;
+        int preMin = A[0];
+        int ans = 0;
+        int maxAns = 0;
+
+        for(int i = 1; i< n; i++){
+            ans = A[i] - preMin;
+            maxAns = Math.max(ans, maxAns);
+            preMin = Math.min(preMin, A[i]);
+        }
+        return maxAns;
+    }
+
+    public static void moveZeroes(int[] A){
+        int idx = 0;
+        for(int i = 0; i < A.length; i++){
+            if(A[i] != 0){
+                A[idx] = A[i];
+                idx++;
+            }
+        }
+
+        while (idx < A.length){
+            A[idx++] = 0;
+        }
+    }
+
+    public static int removeDuplicates(int[] nums) {
+
+        if (nums.length == 0)
+            return 0;
+
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+
+    public static int uniqueElements(int[] A){
+        HashSet<Integer> set = new HashSet<>();
+        for(int a : A){
+            set.add(a);
+        }
+        return set.size();
+    }
+
+    public static int majority(int[] A){
+        int n = A[0];
+        int c = 1;
+        for(int i = 1; i < A.length; i++){
+            if(n == A[i]){
+                c++;
+            }else{
+                c--;
+            }
+            if(c == 0){
+                n = A[i];
+                c = 1;
+            }
+        }
+        return n;
+    }
+
+    public static int[] prodExceptOwn(int[] A){
+        int n = A.length;
+        int[] preProd = new int[n];
+
+        preProd[0] = 1;
+
+        for(int i = 1; i < n; i++){
+            preProd[i] = preProd[i-1] * A[i-1];
+        }
+
+        int right = 1;
+        for(int i = n-1; i>=0; i--){
+            preProd[i] = preProd[i] * right;
+            right *= A[i];
+        }
+        return preProd;
+
+    }
+
+
+    public static int longestSubString(String s){
+        int ans = 1;
+        int maxAns = 1;
+        HashSet<Character> set = new HashSet<>();
+        int l = 0;
+        for(int r =0; r < s.length(); r++){
+            while (set.contains(s.charAt(r))){
+                set.remove(s.charAt(l));
+                l++;
+            }
+            set.add(s.charAt(r));
+            ans = r-l+1;
+            maxAns = Math.max(ans, maxAns);
+        }
+        return maxAns;
+
+    }
+
+
+    public void merge(int[] nums1, int m, int[] nums2, int n){
+        int i = m-1;
+        int j = n-1;
+        int k = m+n -1;
+
+        while (i >= 0 && j>= 0){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            }else{
+                nums1[k] = nums2[j];
+                j--;
+            }
+
+            k--;
+        }
+
+        while (j>=0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+
+
+
+
+
 }
+
--- src/trees/binary_tree/BinaryTree.java
+++ src/trees/binary_tree/BinaryTree.java
@@ -1,5 +1,6 @@
 package trees.binary_tree;
 
+import javax.swing.*;
 import java.util.*;
 
 class BinaryTree {
@@ -17,17 +18,39 @@ class BinaryTree {
         tree.left.left = addNewNode(4);
         tree.left.right = addNewNode(5);
         tree.right = addNewNode(3);
+        tree.right.right = addNewNode(6);
 
 
         Node tree2 = addNewNode(1);
         tree2.left = addNewNode(2);
         tree2.left.left = addNewNode(4);
+        tree2.left.left = addNewNode(4);
         tree2.left.right = addNewNode(5);
         tree2.right = addNewNode(3);
 
+        Node BsTree = addNewNode(8);
+
+        BsTree.left = addNewNode(3);
+        BsTree.right = addNewNode(10);
+
+        BsTree.left.left = addNewNode(1);
+        BsTree.left.right = addNewNode(6);
+
+        BsTree.left.right.left = addNewNode(4);
+        BsTree.left.right.right = addNewNode(7);
+
+        BsTree.right.right = addNewNode(14);
+        BsTree.right.right.left = addNewNode(13);
+
+/*                8             <---- Binary Search Tree BsTree
+                /   \
+               3     10
+              / \      \
+             1   6      14
+                / \     /
+               4   7   13
 
 
-/*
                1
               /  \
              /    \
@@ -40,8 +63,8 @@ class BinaryTree {
 
 */
 
-//        System.out.print("InOrder: ");
-//        printInorder(tree);
+        System.out.print("InOrder: ");
+        printInorder(tree);
 //        System.out.println();
 //        System.out.print("PreOrder: ");
 //        printPreorder(tree);
@@ -58,7 +81,20 @@ class BinaryTree {
 
 //        System.out.println(isBalanced(tree));
 
-        System.out.println(isSameTreee(tree, tree2));
+//        System.out.println(isSameTreee(tree, tree2));
+//        System.out.println(maxDepth(tree));
+
+        System.out.println(levelOrder(tree));
+
+        String str = "asdc";
+
+        char[] ch = str.toCharArray();
+
+        Arrays.sort(ch);
+
+        String res = new String(ch);
+
+        System.out.println(res);
 
 
 
@@ -68,8 +104,6 @@ class BinaryTree {
         if (n == null) {
             return;
         }
-
-
         printInorder(n.left);
         System.out.print(n.data + " ");
         printInorder(n.right);
@@ -220,6 +254,32 @@ class BinaryTree {
         return Math.max(leftHeight, rightHeight) +1;
     }
 
+    public static Node invertTree(Node root){
+        if(root == null) return root;
+
+        Node temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public static boolean isMirror(Node left, Node right){
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        return  left.data == right.data
+                && isMirror(left.left, right.right)
+                && isMirror(left.right, right.left);
+    }
+
+    public static boolean isSymmetric(Node root){
+        if(root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+
     public static boolean isSameTreee(Node A, Node B){
         if(A == null && B == null) return true;
 
@@ -260,5 +320,184 @@ class BinaryTree {
         inOrderWithNull(n.right, arr);
 
     }
+
+
+    int diameter = 0;
+
+    public int diameterOfBinaryTree(Node root) {
+        heights(root);
+        return diameter;
+    }
+
+    private int heights(Node node) {
+        if (node == null)
+            return 0;
+
+        int left = heights(node.left);
+        int right = heights(node.right);
+
+        diameter = Math.max(diameter, left + right);
+
+        return 1 + Math.max(left, right);
+    }
+
+
+
+
+
+
+    ///--------------------------------------------------------------------
+
+
+
+    public static int maxDepth(Node root){
+        if (root == null) return 0;
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) +1;
+    }
+
+    public static boolean isValidBST(Node root){
+        ArrayList<Integer> s = new ArrayList<>();
+        inOrder(root, s);
+
+        for(int i = 1; i < s.size(); i++){
+            if(s.get(i) < s.get(i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isValidBSTN(Node root) {
+        return check(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private static boolean check(Node node, long min, long max) {
+
+        if (node == null)
+            return true;
+
+        if (node.data <= min || node.data >= max)
+            return false;
+
+        return check(node.left, min, node.data) &&
+                check(node.right, node.data, max);
+    }
+
+    public static void inOrder(Node root, ArrayList<Integer> s){
+        if(root == null) return;
+        inOrder(root.left, s);
+        s.add(root.data);
+        inOrder(root.right, s);
+    }
+
+
+    public static ArrayList<ArrayList<Integer>> levelOrder(Node root){
+        if(root == null) return new ArrayList<>();
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        while (!q.isEmpty()){
+            int size = q.size();
+            ArrayList<Integer> temp = new ArrayList<>();
+            while (size > 0){
+                Node t = q.poll();
+                temp.add(t.data);
+
+                if(t.left != null){
+                    q.add(t.left);
+                }
+
+                if(t.right != null){
+                    q.add(t.right);
+                }
+
+                size--;
+            }
+            ans.add(temp);
+        }
+
+        return ans;
+    }
+
+
+    public static boolean hasPathSum(Node root, int T){
+        if(root == null) return false;
+
+        if(root.left == null && root.right == null){
+            return root.data == T;
+        }
+
+        return hasPathSum(root.left, T - root.data) ||
+                hasPathSum(root.right, T - root.data);
+    }
+
+    public static boolean isValidBSTree(Node root){
+        return checkLR(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+
+    public static boolean checkLR(Node root, long min, long max){
+        if(root == null) return true;
+
+        if(root.data <= min || root.data >= max) return false;
+
+        return checkLR(root.left, min, root.data) &&
+                checkLR(root.right, root.data, max);
+    }
+
+    static int count = 0;
+    static int ans = -1;
+
+
+    public static int kthSmallest(Node root, int k){
+        count =0;
+        ans = -1;
+        inOrders(root, k);
+        return ans;
+    }
+
+    public static void inOrders(Node root, int k){
+        if(root == null) return;
+
+
+        inOrders(root.left, k);
+        count++;
+        if(count == k){
+            ans = root.data;
+            return;
+        }
+        inOrders(root.right, k);
+
+    }
+
+    public static int kthLargest(Node root, int k ){
+        count = 0;
+        ans = -1;
+        reverseOrder(root, k);
+        return ans;
+    }
+
+    public static void reverseOrder(Node root, int k){
+        if (root == null) return;
+
+
+        reverseOrder(root.right, k);
+        count++;
+        if(count == k){
+            ans = root.data;
+            return;
+        }
+
+        reverseOrder(root.left, k);
+
+    }
+
+
+
+
+
+
+
 }
 
